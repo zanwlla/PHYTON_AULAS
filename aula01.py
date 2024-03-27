0dcb27ace472876205e24bef90e89ce01010ce8d
Camila <zanellacamila016@gmail.com>
2024-03-27 10:39:05 -0300

    subir aula 01

---
+++ aula01.py
@@ -0,0 +1,11 @@
+import os
+
+#print("Olá mundo cruel")
+print("O 3DS é a turma número 1 do Sagrada")
+turma="2DS"
+pos=2
+print ("o",turma, "é a turma número",pos,"do Sagrada")
+print(type(turma))
+print(type(os))
+#os.system("cls")
+
